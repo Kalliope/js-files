51382a85749f8ca8b311567468f55bef61ddb5e4
Kerstin Reisinger <kre@limessecurity.com>
2018-01-31 13:07:13 +0100

    add
    added files

---
+++ limes.js
@@ -0,0 +1,4 @@
+var x = document.createElement("P");                    
+var t = document.createTextNode("This is Limes Security Pentesting Input.");    
+x.appendChild(t);                                           
+document.body.appendChild(x);                               
